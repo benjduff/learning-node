d91ae585c9524f3f4676c38525aa7eb5824810cf
Ben Duffy <xmrduff@gmail.com>
2018-03-08 18:46:18 +1100

    Creating and deleting directories using Sync and Async functions from the fs module

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 var events = require('events'); //node module
 var util = require('util'); //node module
+var fs = require('fs');
 
+
+/*
 var Person = function(name){ //Create Person object and set name
   this.name = name;
 }
@@ -20,4 +23,28 @@ people.forEach(function(person){ //foreach person in people
 })
 
 James.emit('speak', 'Hello World!'); //speak event listener called, msg is passed to 'speak' callback function(msg)
-Steve.emit('speak', 'Testing custom events!')
+Steve.emit('speak', 'Testing custom events!');
+*/
+
+
+//creating and deleting directories using Sync and Async methods from the fs module
+
+//fs.mkdirSync('newDir');  //Synchronous method for creating a new directory
+//fs.rmdirSync('newDir'); //Synchronous method for deleting a directory
+
+
+//To use the Async method, just delete the word Sync from the method name. An Async method should include a callback func to execute something after the action has been completed. In this case the newDir is created, the readMe.txt file is read, writeMe.txt is then created in the newDir and the data from readMe.txt is written to writeMe.txt
+
+/*
+fs.mkdir('newDir', function(){
+  fs.readFile('../readMe.txt', 'utf8', function(err, data){
+    fs.writeFileSync('./newDir/writeMe.txt', data); //Since writeFile does not have a callback function and readFile is an Async function there is no need for writeFile to be Async too.
+  });
+});
+*/
+
+
+//To remove a directory. A directory must first be empty.
+fs.unlink('./newDir/writeMe.txt', function(){ //unlink removes a particular file in a dir, once that is done execute callback func which deletes the dir.
+  fs.rmdirSync('newDir');
+})
