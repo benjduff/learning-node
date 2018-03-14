d7b4e98f2731c472f2036ac13206bb9cd5a1f000
Ben Duffy <xmrduff@gmail.com>
2018-03-14 22:58:30 +1100

    Installed and basic functionality of ejs, serving html pages with dummy data ect..

--- public/js/expressapp.js
+++ public/js/expressapp.js
@@ -1,15 +1,20 @@
 var express = require('express');
-
-
+var path = require('path');
 var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
 
 app.get('/', function(req, res){
   res.send('this is the homepage');
 })
 
+app.get('/contact', function(req, res){
+  res.sendFile(path.resolve('public/contact.html'));
+})
 
-
-app.get('/profile/:id', function(req, res){
-  res.send('Profile ID: ' + req.params.id);
+app.get('/profile/:name', function(req, res){
+  var data = {age: 29, job: 'Postman', hobbies: ['Snowboarding', 'Poker', 'Blacksmithing']}
+  res.render('profile', {person: req.params.name, data: data})
 })
 app.listen(3000);
